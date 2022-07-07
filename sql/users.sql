69cff9366f96ae07a14661c3597c00fe09754e1a
anumshka <anushka.mpathak17@gmail.com>
2022-07-07 14:33:43 +0530

    users

---
+++ sql/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users( name, email , password , job_role , company , phone )
+VALUES
+( 'sample' , 'sample@gmail.com' , 'sample' , 'company' , 'sample', '12345678'),
+( 'sample2' , 'sample2@gmail.com' , 'sample2' , 'company' , 'sample2', '123456782');
