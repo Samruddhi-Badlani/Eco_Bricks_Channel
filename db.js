5d01aaa4d00f61c19ed604a08d5a898aeec92bc8
anumshka <anushka.mpathak17@gmail.com>
2022-07-07 13:43:15 +0530

    .env changes

--- db.js
+++ db.js
@@ -1,15 +1,8 @@
-const { Pool } = require('pg')
-
+const Pool = require('pg').Pool
+require('dotenv').config()
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const pool = new Pool({
-  user: 'ecobricks',
-  host: 'localhost',
-  database: 'ecobricks',
-  port: 5432,
-  password: 'ecobricks',
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 })
-
-module.exports = {
-  query: (queryText, params, callback) => {
-    return pool.query(queryText, params, callback)
-  },
-}
+module.exports = pool
