60128226707af62e0863c8675e654e6cf84db0e3
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-07-07 16:25:44 +0530

    finalized the project

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 })
 module.exports = pool
